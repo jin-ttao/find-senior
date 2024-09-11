94842171bd46096f3c76c6e4b3169e5eed47fe98
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2024-09-11 17:55:55 +0900

    fix: 테스트 작업 갱신

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+import { sortArray } from "./sorting.js";
 
+const inputNumbers = document.querySelector("#userInputNumbers");
+const inputNumbersButton = document.querySelector("#userInputNumbersButton");
+const graphBoxs = document.querySelectorAll(".graphBox");
+
+let numbersArr = [];
+
+const saveNumbers = function () {
+    numbersArr = inputNumbers.value.split(',').map((num) => {
+        return parseInt(num.trim(), 10);
+    });
+
+    const isAllNumber = numbersArr.every((num) => {
+        return typeof num === 'number' && !isNaN(num);
+    });
+
+    if (isAllNumber) {
+        renderNumberGraph(numbersArr);
+        sortArray(numbersArr).then(result => console.log(result));
+    } else {
+        inputNumbers.value = null;
+    }
+};
+
+const renderNumberGraph = function (array) {
+    array.forEach((number, index) => {
+        const graphNew = document.createElement("div");
+        graphNew.classList.add("graph");
+        graphNew.style.height = number + "px";
+        graphBoxs[index].appendChild(graphNew);
+    });
+};
+
+inputNumbersButton.addEventListener("click", saveNumbers);
+
+  
+// const graphBox = document.querySelector(".graph");
+// graphBoxs[0].style.transform = "translateX(100px)";
--- src/sorting.js
+++ src/sorting.js
@@ -1,3 +1,4 @@
+
 /**
  * 입력 받은 숫자의 배열을 받아 오름차순으로 정렬하여 반환함.
  * 정렬 단계마다 해당 바들의 위치가 변경되는 애니메이션이 보여짐
@@ -5,7 +6,7 @@
  * @param {array} array 입력 받은 숫자의 배열
  * @returns {array} 오름차순으로 정렬된 숫자의 배열
  */
-async function sortArray(array) {
+export async function sortArray(array) {
     const sortedArray = array;
     for (let currentIndex = 1; currentIndex < sortedArray.length; currentIndex++) {
         const currentVacoPeople = sortedArray[currentIndex];
@@ -20,7 +21,7 @@ async function sortArray(array) {
     }
     return sortedArray;
 }
-  
+
 /**
  * 딜레이를 넣어서 await이 정상적으로 작동하는지 테스트 하기위한 stub
  * 파라미터는 언제든지 변경 가능
@@ -29,16 +30,17 @@ async function sortArray(array) {
  * @param {number} compareIndex 
  */
 function animate(currentIndex, compareIndex) {
-sleep(1000);
-console.log(currentIndex, compareIndex);
+    // const graphs = document.querySelectorAll('.graph');
+    // graphs[0].style.cssText = 'transform: translateX(1px); transition-delay: 1s;'
+    sleep(1000);
+    console.log(currentIndex, compareIndex);
 }
 
 function sleep(ms) {
-const wakeUpTime = Date.now() + ms;
-while (Date.now() < wakeUpTime) {}
+    const wakeUpTime = Date.now() + ms;
+    while (Date.now() < wakeUpTime) {}
 }
 
-const exampleArray = [5, 2, 3, 1, 4];
+// const exampleArray = [5, 2, 3, 1, 4];
 
-sortArray(exampleArray).then(result => console.log(result));
-  
+// sortArray(exampleArray).then(result => console.log(result));
