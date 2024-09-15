32f8f7fe5a6c2db9d79470a0ded4f5e7a31a7a67
Jintae Song <w.jinttao@gmail.com>
2024-09-15 17:27:29 +0900

    feat: 입력값 validation 조건 추가(공백 포함 및 소수, 음수 불가하도록 수정, 최대값 9999로 지정), 유저에게 입력값 가이드 해주는 디폴트 메시지 안내 추가

--- src/js/app.js
+++ src/js/app.js
@@ -2,19 +2,34 @@ import { sortNumbers } from "./sorting.js";
 
 const inputNumbers = document.querySelector("#userInputNumbers");
 const inputNumbersButton = document.querySelector("#userInputNumbersButton");
-const graphBoxs = document.querySelectorAll(".graphBox");
 const messageForUserText = document.querySelector("#messageForUserText");
 
 let numbersArr = [];
+messageForUserText.textContent = "1 부터 9999 까지 숫자들을 입력하면 오름차순으로 정렬 돼요!"
 
 const saveNumbers = async function () {
   numbersArr = inputNumbers.value.trim().split(',').map(Number);
+  const isBlankValue = inputNumbers.value.trim().split(',').includes('');
+  const isBlankBetweenNumbers = inputNumbers.value.trim().split('').includes('');
+  const isPrimeNumbers = inputNumbers.value.trim().split('').includes('.');
+  const isAllPositiveNumbersInRange = numbersArr.every((num) => {
+    if (num <= 0 || num >= 10000) {
+      return false;
+    }
 
-  const isAllNumber = numbersArr.every((num) => {
     return typeof num === 'number' && !isNaN(num);
   });
 
-  if (isAllNumber) {
+  if (isBlankValue || isBlankBetweenNumbers || isPrimeNumbers || !isAllPositiveNumbersInRange) {
+    inputNumbers.value = null;
+
+    messageForUserText.style.color = "red";
+    messageForUserText.textContent = "0 보다 큰 정수를 입력해 주세요!";
+
+    return;
+  }
+
+  if (isAllPositiveNumbersInRange) {
     inputNumbers.value = numbersArr;
     messageForUserText.textContent="";
     inputNumbersButton.removeEventListener("click", saveNumbers);
@@ -23,13 +38,9 @@ const saveNumbers = async function () {
     await sortNumbers(numbersArr);
 
     inputNumbersButton.addEventListener("click", saveNumbers);
-  } else {
-    inputNumbers.value = null;
 
-    messageForUserText.style.color = "red";
-    messageForUserText.textContent = "숫자를 입력해 주세요.";
     return;
-  }
+  } 
 };
 
 const renderNumberGraph = function (array) {
